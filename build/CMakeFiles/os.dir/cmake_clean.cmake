4a25904d846282d9f967ebe6139ce0945790e404
samalouty <128055267+samalouty@users.noreply.github.com>
2024-04-18 17:14:45 +0200

    CMakeLists.txt done

---
+++ build/CMakeFiles/os.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/os.dir/os.c.obj"
+  "CMakeFiles/os.dir/os.c.obj.d"
+  "libos.dll.a"
+  "os.exe"
+  "os.exe.manifest"
+  "os.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/os.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
