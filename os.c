0b86201be2e3950a8702347b2a9c87484b6236b8
Aby293 <abdelrahmanelaby@gmail.com>
2024-04-18 15:13:40 +0200

    no progresss

--- os.c
+++ os.c
@@ -1,5 +1,5 @@
 
-
+#define _GNU_SOURCE
 #include <stdio.h>
 #include <stdlib.h>
 #include <pthread.h>
@@ -8,14 +8,20 @@
 #include <sys/time.h>
 #include <sys/resource.h>
 
+cpu_set_t cpu_set;
+
 void *thread_function(void *arg) {
 
+    if(pthread_setaffinity_np(pthread_self(), sizeof(cpu_set_t), &cpu_set)!=0){
+        printf("affinity errorrrr\n");
+    }
+    
     int thread_id = *((int *) arg); // Extract thread ID from argument
     printf("Thread %d: Starting\n", thread_id);
 
     for (int i = 0; i < 3; i++) {
         printf("Thread %d: Executing iteration %d\n", thread_id, i+1);
-        sleep(1);
+        //sleep(1);
     }
     
     printf("Thread %d: Exiting\n", thread_id);
@@ -26,22 +32,53 @@ int main() {
     pthread_attr_t attr;
     pthread_attr_init(&attr);
     struct sched_param param;
-    param.sched_priority = 0;
-
-    // Experiment with different scheduling policies
-     //pthread_attr_setschedpolicy(&attr, SCHED_FIFO); // FIFO
-    pthread_attr_setschedpolicy(&attr, SCHED_RR);   // Round Robin
-    //pthread_attr_setschedpolicy(&attr, SCHED_OTHER); // Default
+    param.sched_priority = 1;
 
     pthread_t threads[4];
     int thread_ids[4] = {1, 2, 3, 4}; // Thread IDs
+    
+    //Create a cpu set for the threads to use
+    CPU_ZERO(&cpu_set);
+    CPU_SET(0, &cpu_set);
+
+    if(pthread_setaffinity_np(pthread_self(), sizeof(cpu_set_t), &cpu_set)!=0){
+        printf("affinity error\n");
+    }
+    
+    if(pthread_attr_setinheritsched(&attr,PTHREAD_EXPLICIT_SCHED)!=0){
+        printf("inherit error\n");
+    }
+
+    if(pthread_setschedparam(pthread_self(),SCHED_RR,&param)!=0){
+        printf("main sched error\n");
+    }
+
+   
+    if(pthread_attr_setschedpolicy(&attr, SCHED_RR)!=0){
+        printf("attr policy error\n");
+    }
+    if(pthread_attr_setschedparam(&attr,&param)!=0){
+        printf("attr param error\n");
+    }
+    
+
 
     // Create threads with specified scheduling policy
-    for (int i = 0; i < 4; i++) {
-        printf("Creating a thread\n");
-        pthread_create(&threads[i], &attr, thread_function, (void *) &thread_ids[i]);
-        //sleep(1);
-    }
+    // for (int i = 0; i < 4; i++) {
+    //     printf("Creating a thread\n");
+    //     pthread_create(&threads[i], &attr, thread_function, (void *) &thread_ids[i]);
+        
+    //     //sleep(1);
+    // }
+
+    printf("Creating a thread\n");
+    pthread_create(&threads[0], &attr, thread_function, (void *) &thread_ids[0]);
+    printf("Creating a thread\n");
+    pthread_create(&threads[1], &attr, thread_function, (void *) &thread_ids[1]);
+    printf("Creating a thread\n");
+    pthread_create(&threads[2], &attr, thread_function, (void *) &thread_ids[2]);
+    printf("Creating a thread\n");
+    pthread_create(&threads[3], &attr, thread_function, (void *) &thread_ids[3]);
 
     // Join threads
     for (int i = 0; i < 4; i++) {
